2069d47b73888d0a0f0eae11057eea143f84f1c7
Desheng Liu <deshengliu@yahoo.com>
2025-01-13 21:12:53 -0600

    updated this branch

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import "./Navbar.scss";
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            {/*Sidebar*/}
+            <div className="wrapper">
+                <span>
+                    Desheng Liu
+                </span>
+                <div className="social">
+                    <a href="#"><img src="/facebook.png" alt="" /></a>
+                    <a href="#"><img src="/instagram.png" alt="" /></a>
+                    <a href="#"><img src="/youtube.png" alt="" /> </a>
+                </div>
+            </div>
+        </div>
+
+    );
+};
+
+export default Navbar
